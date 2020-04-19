2ec809a3746ad81be64c2dc31e76166e8311a9c5
최창준 <aowert6518@naver.com>
2020-04-19 21:41:07 +0900

    language, page class 수정
    언어가 책의 부모로 foreignkey쓰이는게 더 좋을 것 같고 page클래스가 onetoone 로 하는게 좋을 것 같아서 수정

--- cj/base/models.py
+++ cj/base/models.py
@@ -8,9 +8,23 @@ class Library(models.Model):
     address = models.CharField(max_length = 200)
 
 
+    def __str__(self):
+        return self.name
+
+
 class Author(models.Model):
     name = models.CharField(max_length = 200)
     nationarity = models.CharField(max_length = 200)
+
+    def __str__(self):
+        return self.name
+
+class Language(models.Model):
+
+    language = models.CharField(max_length = 200)
+
+    def __str__(self):
+        return self.language
 
 
 class Book(models.Model):
@@ -24,15 +38,23 @@ class Book(models.Model):
         on_delete = models.CASCADE,
         related_name = 'books'
     )
+    language = models.ForeignKey(
+        Language,
+        on_delete = models.CASCADE,
+        related_name = 'books'
+    )
 
     title = models.CharField(max_length = 200)
+
+    def __str__(self):
+        return self.title
 
 
 # 책이 어느 도서관에 몇개 있는지에 대한 걸 만들 순 없는가?
 
 
 class Page(models.Model):
-    book = models.ForeignKey(
+    book = models.OneToOneField(
         Book,
         on_delete = models.CASCADE,
         related_name = 'pages',
@@ -41,14 +63,7 @@ class Page(models.Model):
     text = models.TextField(null=True, blank = True)
     page_number = models.IntegerField()
 
+    def __str__(self):
+        return self.page_number
 
-
-class Language(models.Model):
-    book = models.OneToOneField(
-        Book,
-        on_delete = models.CASCADE,
-        related_name = 'languages',
-
-    )
-
-    language = models.CharField(max_length = 200)
+ 
