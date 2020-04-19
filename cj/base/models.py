0e3d412dc5f3b967ade64dae4f7060ac927160f9
최창준 <aowert6518@naver.com>
2020-04-19 16:37:13 +0900

    모델 클래스 구상
    테이블?이 될 수 있는 것들을 만들어줌

--- cj/base/models.py
+++ cj/base/models.py
@@ -3,3 +3,18 @@ from django.db import models
 # Create your models here.
 
 
+class Library(models.Model):
+    pass
+
+class Author(models.Model):
+    pass
+
+
+class Book(modles.Model):
+    pass
+
+class Page(models.Model):
+    pass
+
+class Language(models.Model):
+    pass
