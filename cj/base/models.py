224d01a69823c6034fa487df6c7127b8caf41c22
최창준 <aowert6518@naver.com>
2020-04-21 12:01:36 +0900

    기본 모델 구상
    필요한 모델 클래스 만들어주기

--- cj/base/models.py
+++ cj/base/models.py
@@ -2,4 +2,19 @@ from django.db import models
 
 # Create your models here.
 
+class Library(models.Model):
+    
 
+
+class Author(models.Model):
+    
+
+
+class Language(models.Model):
+
+
+class Book(models.Model):
+
+
+class Page(model.Model):
+    
