6b9ed1f4566821615f7974a96b54bb86b329b78d
최창준 <aowert6518@naver.com>
2020-04-21 12:08:15 +0900

    모델에 필요한 필드 구성하기

--- cj/base/models.py
+++ cj/base/models.py
@@ -3,18 +3,38 @@ from django.db import models
 # Create your models here.
 
 class Library(models.Model):
-    
+    name = models.CharField(max_length = 200)
 
 
 class Author(models.Model):
-    
+    name = models.CharField(max_length = 200)
 
 
 class Language(models.Model):
-
+    name = models.CharField(max_length = 200)
 
 class Book(models.Model):
+    library = models.ForeignKey(
+        Library,
+        on_delete = models.CASCADE,
+    )
+    author = models.ForeignKey(
+        Author,
+        on_delete = models.CASCADE,
+    )
+    language = models.ForeignKey(
+        Language,
+        on_delete = models.CASCADE,
+    )
+
+    title = models.CharField(max_length=200)
+    
 
 
 class Page(model.Model):
-    
+    book = models.OneToOneField(
+        Book,
+        on_delete = models.CASCADE,
+    )
+
+    page_length = models.IntegerField()
